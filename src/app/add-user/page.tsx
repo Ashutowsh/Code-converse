4ef9858977c719fa5c4b2aaa87803b69662dea2c
Ashutosh <Ashutosh@LAPTOP-N00785I6>
2025-01-06 22:29:15 +0530

    Add user details to app db

---
+++ src/app/add-user/page.tsx
@@ -0,0 +1,42 @@
+import { auth, clerkClient } from '@clerk/nextjs/server'
+import { notFound, redirect } from 'next/navigation';
+import prisma from '../../../lib/prisma';
+
+async function AddUser() {
+
+    const {userId} = await auth();
+    if(!userId) notFound();
+
+    const client = await clerkClient();
+    const user = await client.users.getUser(userId);
+
+    if(!user.emailAddresses[0]?.emailAddress) 
+        return new Error('User email not found');
+
+    try {
+        const createdUser = await prisma.user.upsert({
+            where: {
+              email: user.emailAddresses[0].emailAddress ?? "",
+            },
+            update: {
+              imageUrl: user.imageUrl,
+              firstName: user.firstName,
+              lastName: user.lastName
+            },
+            create: {
+                id: user.id,
+                email: user.emailAddresses[0].emailAddress,
+                imageUrl: user.imageUrl,
+                username: user.username ?? "",
+                firstName: user.firstName,
+                lastName: user.lastName,
+            },
+          })
+    } catch (error:any) {
+        return new Error("Error in adding user details : ", error.message);
+    }
+
+  return redirect('/dashboard');
+}
+
+export default AddUser
