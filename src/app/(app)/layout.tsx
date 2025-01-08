c13a30aff16437b6d9178a08addfbfcce14657d2
Ashutosh <Ashutosh@LAPTOP-N00785I6>
2025-01-08 17:48:08 +0530

    Complete Sidebar and add dark backgound

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,29 +1,25 @@
-import SideComponent from "@/components/SideComponent";
+import {SideComponent} from "@/components/SideComponent";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { UserButton } from "@clerk/nextjs";
 
 export default function AppLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-      <html lang="en">
-        <body>
-          <SidebarProvider>
-            <SideComponent />
-            <main className="w-full m-2">
-              <div className="flex items-center gap-2 border-sidebar bg-sidebar border shadow rounded-md p-2 px-4">
-                  <div className="ml-auto"></div>
-                  <UserButton />
-              </div>
-              <div className="h-4"></div>
-              <div className="border-sidebar-border bg-sidebar border shadow rounded-md overflow-y-scroll h-[calc(100vh - 6rem)] p-4">
-                  {children}
-              </div>
-            </main>
-          </SidebarProvider>
-        </body>
-      </html>
+    <SidebarProvider>
+      <SideComponent />
+        <main className="w-full m-2">
+          <div className="flex items-center gap-2 border-sidebar bg-sidebar border shadow rounded-md p-2 px-4">
+            <div className="ml-auto"></div>
+              <UserButton />
+            </div>
+            <div className="h-4"></div>
+            <div className="border-sidebar-border bg-sidebar border shadow rounded-md overflow-y-scroll h-[calc(100vh - 6rem)] p-4">
+                {children}
+            </div>
+        </main>
+    </SidebarProvider>
   );
 }
--- src/components/SideComponent.tsx
+++ src/components/SideComponent.tsx
@@ -1,14 +1,78 @@
-'use client'
+'use client';
 
-import React from 'react'
-import { Sidebar } from './ui/sidebar'
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar } from './ui/sidebar';
+import { sidebarItems, projects } from '@/constants';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { Button } from './ui/button';
+import { Plus } from 'lucide-react';
 
-function SideComponent() {
+export function SideComponent() {
+  const pathname = usePathname();
+  const {open} = useSidebar();
+  console.log("Pathname :", pathname)
   return (
-    <Sidebar collapsible='icon' variant='floating'>
-      Hello World
-    </Sidebar>
-  )
-}
+    <Sidebar collapsible="icon" variant="floating">
+      <SidebarHeader>Logo</SidebarHeader>
 
-export default SideComponent
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {sidebarItems.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url} className={cn({
+                      '!bg-primary !text-white': pathname === item.url
+                    }, 'list-none')}>
+                    <item.icon />
+                    <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+
+        <SidebarGroup>
+          <SidebarGroupLabel>
+            Your Projects
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {projects.map((project) => (
+                <SidebarMenuItem key={project.name}>
+                  <SidebarMenuButton asChild>
+                    <div>
+                      <div className={cn(
+                        'rounded-sm border size-6 flex items-center justify-center text-sm bg-white text-primary',
+                        {'bg-primary text-white': true}
+                      )}>
+                        {project.name[0]}
+                      </div>
+                      <span>{project.name}</span>
+                    </div>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+
+              <div className='h-2'></div>
+
+            {open && (<SidebarMenuItem>
+                <Link href="/create-project">
+                  <Button size='sm' variant='outline' className='w-fit'>
+                    <Plus />
+                    Create project
+                  </Button>
+                </Link>
+              </SidebarMenuItem>)}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
